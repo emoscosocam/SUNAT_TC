78b2c1b447d81e87c0c7025a9b65dfa49b638e9f
Ernesto Moscoso Cam <emoscosocam@users.noreply.github.com>
2017-04-01 14:54:28 -0500

    Add files via upload

---
+++ SUNAT_TC_WinForms/frmTest.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataSet1 = New System.Data.DataSet()
+        Me.DataTable1 = New System.Data.DataTable()
+        Me.DataColumn1 = New System.Data.DataColumn()
+        Me.DataColumn2 = New System.Data.DataColumn()
+        Me.DataColumn3 = New System.Data.DataColumn()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.FechaDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CompraDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VentaDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BtnCargar = New System.Windows.Forms.Button()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "NewDataSet"
+        Me.DataSet1.Tables.AddRange(New System.Data.DataTable() {Me.DataTable1})
+        '
+        'DataTable1
+        '
+        Me.DataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.DataColumn1, Me.DataColumn2, Me.DataColumn3})
+        Me.DataTable1.TableName = "Table1"
+        '
+        'DataColumn1
+        '
+        Me.DataColumn1.Caption = "Fecha"
+        Me.DataColumn1.ColumnName = "Fecha"
+        Me.DataColumn1.DataType = GetType(Date)
+        '
+        'DataColumn2
+        '
+        Me.DataColumn2.Caption = "Compra"
+        Me.DataColumn2.ColumnName = "Compra"
+        Me.DataColumn2.DataType = GetType(Double)
+        '
+        'DataColumn3
+        '
+        Me.DataColumn3.Caption = "Venta"
+        Me.DataColumn3.ColumnName = "Venta"
+        Me.DataColumn3.DataType = GetType(Double)
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.FechaDataGridViewTextBoxColumn, Me.CompraDataGridViewTextBoxColumn, Me.VentaDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataMember = "Table1"
+        Me.DataGridView1.DataSource = Me.DataSet1
+        Me.DataGridView1.Location = New System.Drawing.Point(11, 62)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(616, 446)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'FechaDataGridViewTextBoxColumn
+        '
+        Me.FechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha"
+        Me.FechaDataGridViewTextBoxColumn.HeaderText = "Fecha"
+        Me.FechaDataGridViewTextBoxColumn.Name = "FechaDataGridViewTextBoxColumn"
+        Me.FechaDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'CompraDataGridViewTextBoxColumn
+        '
+        Me.CompraDataGridViewTextBoxColumn.DataPropertyName = "Compra"
+        Me.CompraDataGridViewTextBoxColumn.HeaderText = "Compra"
+        Me.CompraDataGridViewTextBoxColumn.Name = "CompraDataGridViewTextBoxColumn"
+        Me.CompraDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'VentaDataGridViewTextBoxColumn
+        '
+        Me.VentaDataGridViewTextBoxColumn.DataPropertyName = "Venta"
+        Me.VentaDataGridViewTextBoxColumn.HeaderText = "Venta"
+        Me.VentaDataGridViewTextBoxColumn.Name = "VentaDataGridViewTextBoxColumn"
+        Me.VentaDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'BtnCargar
+        '
+        Me.BtnCargar.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.BtnCargar.Location = New System.Drawing.Point(19, 13)
+        Me.BtnCargar.Name = "BtnCargar"
+        Me.BtnCargar.Size = New System.Drawing.Size(117, 43)
+        Me.BtnCargar.TabIndex = 0
+        Me.BtnCargar.Text = "Cargar"
+        Me.BtnCargar.UseVisualStyleBackColor = True
+        '
+        'frmTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(636, 514)
+        Me.Controls.Add(Me.BtnCargar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "frmTest"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Test SUNAT TC"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataSet1 As DataSet
+    Friend WithEvents DataTable1 As DataTable
+    Friend WithEvents DataColumn1 As DataColumn
+    Friend WithEvents DataColumn2 As DataColumn
+    Friend WithEvents DataColumn3 As DataColumn
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents FechaDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CompraDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents VentaDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BtnCargar As Button
+End Class
---
+++ SUNAT_TC_WinForms/frmTest.vb
@@ -0,0 +1,161 @@
+﻿Imports System.Text.RegularExpressions ' Se importa esto para poder usar el método Regex.Split()
+Public Class frmTest
+
+
+    ' >>> Escrito por Ernesto Moscoso Cam
+    ' >>>   Abril 2017
+    ' >>>
+    ' >>>     Basado en el código C# hecho por Afu Nang Tse Mundaca: http://r3xet.blogspot.pe/2013/12/obtener-el-tipo-de-cambio-de-sunat-del.html
+
+
+
+    Private Sub BtnCargar_Click(sender As Object, e As EventArgs) Handles BtnCargar.Click
+
+        ' Se manda poblar la tabla...
+        ObtenerDatos(Me.DataTable1)
+
+    End Sub
+
+
+
+
+    Private Sub ObtenerDatos(ByRef objDataTable As Data.DataTable)
+        Dim objWebClient As System.Net.WebClient
+        Dim strHTML As String = ""
+        Dim strURL As String
+
+        Dim intAño As Integer
+        Dim intMes As Integer
+
+        Dim dtDateAux As Date
+
+        objWebClient = New System.Net.WebClient()
+
+        dtDateAux = DateSerial(Year(Now), Month(Now), 1).AddDays(-1) ' Este es el último día del mes anterior
+
+        ' Primero: Obtener el TC del mes anterior
+        intAño = Year(dtDateAux)
+        intMes = Month(dtDateAux)
+
+        ' La página del TC de la SUNAT acepta un Query String donde se le indica el año y el mes
+        strURL = "http://www.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes=" & intMes & "&anho=" & intAño
+
+        If CargarDocHTML(objWebClient, strURL, strHTML) Then ' Si la función retorna falso, entonces no se procesa
+            ProcesarDocHTML(strHTML, intAño, intMes, objDataTable)
+        End If
+
+        ' Segundo: Obtener el TC del mes en curso
+        intAño = Year(Now())
+        intMes = Month(Now())
+
+        strURL = "http://www.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes=" & intMes & "&anho=" & intAño
+
+        If CargarDocHTML(objWebClient, strURL, strHTML) Then ' Si la función retorna falso, entonces no se procesa
+            ProcesarDocHTML(strHTML, intAño, intMes, objDataTable)
+        End If
+
+        objWebClient.Dispose()
+
+        objDataTable.AcceptChanges() ' Commit
+    End Sub
+
+    ' Esta rutina sólo se encarga de conectarse a la web y obtener el HTML
+    Private Function CargarDocHTML(ByRef objWebClient As System.Net.WebClient, ByVal strURL As String, ByRef strHTML As String) As Boolean
+        Dim bAux As Boolean = False
+
+        Try
+            strHTML = objWebClient.DownloadString(strURL) ' Esto podría fallar (debido a problemas con la red, o que el servidor esté fuera de linea, etc)
+
+            bAux = Not (InStr(strHTML, "No existe Informaci") > 0) ' Esta función retorna False si no hay datos
+        Catch ex As Exception
+            ' Si hay una excepción no se hace nada
+        End Try
+
+        Return bAux
+    End Function
+
+    ' Esta rutina "procesa" el HTML como String
+    ' Lo que hace es recorrer el código HTML de la página y obtener de allí subcadenas...
+    Private Sub ProcesarDocHTML(strHTML As String, intAño As Integer, intMes As Integer, ByRef objDataTable As Data.DataTable)
+        Dim strTable As String
+        Dim strFilas As String()
+        Dim strCeldas As String()
+        Dim intFirstPosition As Integer
+        Dim intLastPosition As Integer
+        Dim i As Integer
+        Dim j As Integer
+
+        Dim dtFecha As Date
+        Dim dblCompra As Double
+        Dim dblVenta As Double
+
+        intFirstPosition = InStr(strHTML, "form-table") ' Primero se obtenie la posición de la cadena "form-table"
+        intFirstPosition = InStr(intFirstPosition, strHTML, "<tr>") - 1 ' Una vez que el elemento table se ha localizado, se avanza el puntero hasta la primera fila
+        ' En una tabla HTML, las filas se identifican con <tr>
+
+        intLastPosition = InStr(intFirstPosition, strHTML, "</table>") - 7 ' Se obtiene la posición del elemento de cierre "</table>"
+
+        strTable = strHTML.Substring(intFirstPosition, intLastPosition - intFirstPosition) ' Se extrae una subcadena que contiene todas las filas de la tabla
+
+        strFilas = Regex.Split(strTable, "</tr>") ' Se usa Split() para obtener un arreglo de filas. Ojo que la primera fila contiene las cabeceras de la tabla
+
+        For i = 1 To strFilas.GetUpperBound(0)  ' El bucle empieza desde 1 en vez de 0 para saltearse la fila de cabeceras
+            strCeldas = Regex.Split(strFilas(i).Trim, "/td>") 'Se usa Split() para obtener un arrego de celdas
+            ' ...............................................Al dejar el caracter '<' se puede delimitar luego el contenido de la celda
+
+            ' strCeldas tiene TODAS las celdas de una fila, pero se necesitan leer las celdas 3 a la vez... 
+            For j = 0 To strCeldas.GetUpperBound(0) Step 3 'Las celdas siempre aparecen en grupos de 3: ("Dia", "Compra", "Venta")
+                If strCeldas(j) <> "" Then ' La función RegEx.Split a veces deja una cadena nula al final del arreglo. Por eso se itera sólo donde hay datos
+                    ObtenerTriplete(strCeldas, j, intAño, intMes, dtFecha, dblCompra, dblVenta) ' Un triplete se define como (dtFecha, dblCompra, dblVenta)
+                    AddRow(objDataTable, dtFecha, dblCompra, dblVenta)
+                End If
+            Next
+        Next
+    End Sub
+
+    ' Valores posibles para intIndice: 0, 3, 6, 9
+    ' Un triplete es un grupo de 3 celdas, una para la Fecha, otra para el valor de Compra y finalmente otra para el valor de Venta
+    Private Sub ObtenerTriplete(strCeldas As String(), intIndice As Integer, intAño As Integer, intMes As Integer, ByRef dtFecha As Date, ByRef dblCompra As Double, ByRef dblVenta As Double)
+        Dim intDia As Integer
+
+        intDia = CInt(ObtenerValorCelda(strCeldas(intIndice)))
+        dtFecha = DateSerial(intAño, intMes, intDia)
+
+        dblCompra = CDbl(ObtenerValorCelda(strCeldas(intIndice + 1)))
+        dblVenta = CDbl(ObtenerValorCelda(strCeldas(intIndice + 2)))
+    End Sub
+
+    ' Esta función extrae el Valor del elemento <td>
+    Private Function ObtenerValorCelda(strCelda As String) As String
+        Dim strValorCelda As String
+        Dim intFirstPosition As Integer
+        Dim intLastPosition As Integer
+
+        intFirstPosition = InStr(strCelda, "<strong>") ' El elemento "<strong>" sólo se encuentra en la primera columna del triplete ("Dia")
+        If intFirstPosition > 0 Then
+            intLastPosition = InStr(strCelda, "</strong>")
+            intFirstPosition = intFirstPosition + 7
+        Else ' Si el elemento "<strong>" no se ha encontrado, entonces estamos en la columna "Compra" o "Venta"
+            intFirstPosition = InStr(strCelda, "tne10") ' la cadena 'class = "tne10"' sólo se encuentra en las columnas "Compra" o "Venta"
+            intFirstPosition = intFirstPosition + 7
+            intLastPosition = InStr(intFirstPosition, strCelda, "<") ' El caracter '<' fue dejado intencionalmente cuando se usó RegEx.Split() para poderse usarsele como marcador aquí
+        End If
+
+        strValorCelda = strCelda.Substring(intFirstPosition, intLastPosition - intFirstPosition - 1).Trim ' Después de obtenerse las posiciones, se extrae la subcadena del valor
+
+        Return strValorCelda
+    End Function
+
+    ' Agrega una fila en objDataTable
+    Private Sub AddRow(ByRef objDataTable As Data.DataTable, dtFecha As Date, dblCompra As Double, dblVenta As Double)
+        Dim objDataRow As Data.DataRow
+
+        objDataRow = objDataTable.NewRow
+
+        objDataRow(0) = dtFecha
+        objDataRow(1) = dblCompra
+        objDataRow(2) = dblVenta
+
+        objDataTable.Rows.Add(objDataRow)
+    End Sub
+End Class
