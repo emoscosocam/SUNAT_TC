aa04c4f5c16f6218165137c751e397723090e0d7
Ernesto Moscoso Cam <emoscosocam@users.noreply.github.com>
2017-04-02 12:04:17 -0500

    Nuevo Proyecto Test en C#

---
+++ SUNAT_TC_WinForms/CS/frmTest.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SUNAT_TC_Test_CS
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataSet1 = new System.Data.DataSet();
+            this.DataTable1 = new System.Data.DataTable();
+            this.DataColumn1 = new System.Data.DataColumn();
+            this.DataColumn2 = new System.Data.DataColumn();
+            this.DataColumn3 = new System.Data.DataColumn();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompraDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.CmbMes = new System.Windows.Forms.ComboBox();
+            this.CmbAño = new System.Windows.Forms.ComboBox();
+            this.BtnCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "NewDataSet";
+            this.DataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.DataTable1});
+            // 
+            // DataTable1
+            // 
+            this.DataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.DataColumn1,
+            this.DataColumn2,
+            this.DataColumn3});
+            this.DataTable1.TableName = "Table1";
+            // 
+            // DataColumn1
+            // 
+            this.DataColumn1.Caption = "Fecha";
+            this.DataColumn1.ColumnName = "Fecha";
+            this.DataColumn1.DataType = typeof(System.DateTime);
+            // 
+            // DataColumn2
+            // 
+            this.DataColumn2.Caption = "Compra";
+            this.DataColumn2.ColumnName = "Compra";
+            this.DataColumn2.DataType = typeof(double);
+            // 
+            // DataColumn3
+            // 
+            this.DataColumn3.Caption = "Venta";
+            this.DataColumn3.ColumnName = "Venta";
+            this.DataColumn3.DataType = typeof(double);
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataGridView1.AutoGenerateColumns = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FechaDataGridViewTextBoxColumn,
+            this.CompraDataGridViewTextBoxColumn,
+            this.VentaDataGridViewTextBoxColumn});
+            this.DataGridView1.DataMember = "Table1";
+            this.DataGridView1.DataSource = this.DataSet1;
+            this.DataGridView1.Location = new System.Drawing.Point(1, 66);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.ReadOnly = true;
+            this.DataGridView1.Size = new System.Drawing.Size(436, 498);
+            this.DataGridView1.TabIndex = 2;
+            // 
+            // FechaDataGridViewTextBoxColumn
+            // 
+            this.FechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
+            this.FechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
+            this.FechaDataGridViewTextBoxColumn.Name = "FechaDataGridViewTextBoxColumn";
+            this.FechaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // CompraDataGridViewTextBoxColumn
+            // 
+            this.CompraDataGridViewTextBoxColumn.DataPropertyName = "Compra";
+            this.CompraDataGridViewTextBoxColumn.HeaderText = "Compra";
+            this.CompraDataGridViewTextBoxColumn.Name = "CompraDataGridViewTextBoxColumn";
+            this.CompraDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // VentaDataGridViewTextBoxColumn
+            // 
+            this.VentaDataGridViewTextBoxColumn.DataPropertyName = "Venta";
+            this.VentaDataGridViewTextBoxColumn.HeaderText = "Venta";
+            this.VentaDataGridViewTextBoxColumn.Name = "VentaDataGridViewTextBoxColumn";
+            this.VentaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(6, 39);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(30, 13);
+            this.Label2.TabIndex = 10;
+            this.Label2.Text = "Mes:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(6, 15);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(29, 13);
+            this.Label1.TabIndex = 9;
+            this.Label1.Text = "Año:";
+            // 
+            // CmbMes
+            // 
+            this.CmbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbMes.FormattingEnabled = true;
+            this.CmbMes.Location = new System.Drawing.Point(41, 39);
+            this.CmbMes.Name = "CmbMes";
+            this.CmbMes.Size = new System.Drawing.Size(121, 21);
+            this.CmbMes.Sorted = true;
+            this.CmbMes.TabIndex = 8;
+            // 
+            // CmbAño
+            // 
+            this.CmbAño.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbAño.FormattingEnabled = true;
+            this.CmbAño.Location = new System.Drawing.Point(41, 12);
+            this.CmbAño.Name = "CmbAño";
+            this.CmbAño.Size = new System.Drawing.Size(121, 21);
+            this.CmbAño.TabIndex = 7;
+            // 
+            // BtnCargar
+            // 
+            this.BtnCargar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BtnCargar.Location = new System.Drawing.Point(182, 15);
+            this.BtnCargar.Name = "BtnCargar";
+            this.BtnCargar.Size = new System.Drawing.Size(117, 43);
+            this.BtnCargar.TabIndex = 6;
+            this.BtnCargar.Text = "Cargar";
+            this.BtnCargar.UseVisualStyleBackColor = true;
+            this.BtnCargar.Click += new System.EventHandler(this.BtnCargar_Click);
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 567);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.CmbMes);
+            this.Controls.Add(this.CmbAño);
+            this.Controls.Add(this.BtnCargar);
+            this.Controls.Add(this.DataGridView1);
+            this.Name = "frmTest";
+            this.Text = "Test SUNAT TC (C#)";
+            this.Load += new System.EventHandler(this.frmTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Data.DataSet DataSet1;
+        internal System.Data.DataTable DataTable1;
+        internal System.Data.DataColumn DataColumn1;
+        internal System.Data.DataColumn DataColumn2;
+        internal System.Data.DataColumn DataColumn3;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn FechaDataGridViewTextBoxColumn;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn CompraDataGridViewTextBoxColumn;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn VentaDataGridViewTextBoxColumn;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.ComboBox CmbMes;
+        private System.Windows.Forms.ComboBox CmbAño;
+        internal System.Windows.Forms.Button BtnCargar;
+    }
+}
+
---
+++ SUNAT_TC_WinForms/CS/frmTest.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;   // Se importa esto para poder usar el método Regex.Split()
+
+
+namespace SUNAT_TC_Test_CS
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+
+        // Este es un helper method porque DateSerial() no existe en C#
+        private DateTime DateSerial(int intYear, int intMonth, int intDay)
+        {
+            return new DateTime(intYear, intMonth, intDay);
+        }
+
+        private void frmTest_Load(object sender, EventArgs e)
+        {
+            InicializarCombos();
+        }
+        private void InicializarCombos()
+        {
+            int i = 0;
+
+            for (i = 1; i <= 12; i++)
+            {
+                CmbMes.Items.Add(i.ToString("0#"));
+            }
+            CmbMes.Text = DateTime.Now.Month.ToString("0#");
+
+            for (i = DateTime.Now.Year; i >= 1995; i += -1)
+            {
+                CmbAño.Items.Add(i);
+            }
+
+            CmbAño.SelectedIndex = 0;
+        }
+
+        private void BtnCargar_Click(object sender, EventArgs e)
+        {
+            // Se manda poblar la tabla...
+            ObtenerDatos(this.DataTable1, Convert.ToInt32(this.CmbAño.Text), Convert.ToInt32(this.CmbMes.Text));
+        }
+
+
+        private void ObtenerDatos(System.Data.DataTable objDataTable, int intAño, int intMes)
+        {
+            System.Net.WebClient objWebClient;
+            string strHTML = "";
+            string strURL;
+            
+
+            objWebClient = new System.Net.WebClient();
+
+           
+            // La página del TC de la SUNAT acepta un Query String donde se le indica el año y el mes
+            strURL = "http://www.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes=" + intMes + "&anho=" + intAño;
+
+            // Si la función retorna falso, entonces no se procesa
+            if (CargarDocHTML(objWebClient, strURL, ref strHTML))
+            {
+                ProcesarDocHTML(strHTML, intAño, intMes, objDataTable);
+            }
+
+          
+            objWebClient.Dispose();
+
+            objDataTable.AcceptChanges();// Commit
+        }
+
+        // Esta rutina sólo se encarga de conectarse a la web y obtener el HTML
+        private bool CargarDocHTML(System.Net.WebClient objWebClient, string strURL,  ref string strHTML)
+        {
+            bool bAux = false;
+           
+            try
+            {
+                strHTML = objWebClient.DownloadString(strURL);
+                // Esto podría fallar (debido a problemas con la red, o que el servidor esté fuera de linea, etc)
+
+                bAux = !(strHTML.IndexOf("No existe Informaci") > 0);
+                // Esta función retorna False si no hay datos
+            }
+            catch (Exception ex)
+            {
+                // Si hay una excepción no se hace nada               
+            }
+
+            return bAux;
+        }
+
+        // Esta rutina "procesa" el HTML como String
+        // Lo que hace es recorrer el código HTML de la página y obtener de allí subcadenas...
+        private void ProcesarDocHTML(string strHTML, int intAño, int intMes, System.Data.DataTable objDataTable)
+        {
+            string strTable;
+            string[] strFilas;
+            string[] strCeldas;
+            int intFirstPosition;
+            int intLastPosition;
+            int i;
+            int j;
+
+            System.DateTime dtFecha = DateTime.Now; // En C# se debe inicializar la variable para poder pasarle por Referencia
+            double dblCompra = 0.0;
+            double dblVenta = 0.0;
+
+            intFirstPosition = strHTML.IndexOf("form-table");  // Primero se obtiene la posición de la cadena "form-table"
+
+            intFirstPosition = strHTML.IndexOf("<tr>", intFirstPosition) - 1; // Una vez que el elemento table se ha localizado, se avanza el puntero hasta la primera fila
+                                                                              // En una tabla HTML, las filas se identifican con <tr>
+
+            intLastPosition = strHTML.IndexOf("</table>", intFirstPosition) - 7;  // Se obtiene la posición del elemento de cierre "</table>"
+
+            strTable = strHTML.Substring(intFirstPosition, intLastPosition - intFirstPosition);  // Se extrae una subcadena que contiene todas las filas de la tabla
+
+            strFilas = Regex.Split(strTable, "</tr>"); // Se usa Split() para obtener un arreglo de filas. Ojo que la primera fila contiene las cabeceras de la tabla
+
+            // El bucle empieza desde 1 en vez de 0 para saltearse la fila de cabeceras
+            for (i = 1; i <= strFilas.GetUpperBound(0); i++)
+            {
+                strCeldas = Regex.Split(strFilas[i].Trim(), "/td>");  // Se usa Split() para obtener un arrego de celdas
+                                                                    // Al dejar el caracter '<' se puede delimitar luego el contenido de la celda
+
+                // strCeldas tiene TODAS las celdas de una fila, pero se necesitan leer las celdas 3 a la vez... 
+                //Las celdas siempre aparecen en grupos de 3: ("Dia", "Compra", "Venta")
+                for (j = 0; j <= strCeldas.GetUpperBound(0); j += 3)
+                {
+                    // La función RegEx.Split() a veces deja una cadena nula al final del arreglo. Por eso se itera sólo donde hay datos
+                    if (!string.IsNullOrEmpty(strCeldas[j]))
+                    {
+                        ObtenerTriplete(strCeldas, j, intAño, intMes, ref dtFecha, ref dblCompra, ref dblVenta); // Un triplete se define como (dtFecha, dblCompra, dblVenta)
+                        AddRow(objDataTable, dtFecha, dblCompra, dblVenta);
+                    }
+                }
+            }
+        }
+
+        // Valores posibles para intIndice: 0, 3, 6, 9
+        // Un triplete es un grupo de 3 celdas, una para la Fecha, otra para el valor de Compra y finalmente otra para el valor de Venta
+        private void ObtenerTriplete(string[] strCeldas, int intIndice, int intAño, int intMes, ref System.DateTime dtFecha, ref double dblCompra, ref double dblVenta)
+        {
+            int intDia = 0;
+
+            intDia = Convert.ToInt32(ObtenerValorCelda(strCeldas[intIndice]));
+            dtFecha = DateSerial(intAño, intMes, intDia);
+
+            dblCompra = Convert.ToDouble(ObtenerValorCelda(strCeldas[intIndice + 1]));
+            dblVenta = Convert.ToDouble(ObtenerValorCelda(strCeldas[intIndice + 2]));
+        }
+
+        // Esta función extrae el Valor del elemento <td>
+        private string ObtenerValorCelda(string strCelda)
+        {
+            string strValorCelda;
+            int intFirstPosition;
+            int intLastPosition;
+
+            intFirstPosition = strCelda.IndexOf("<strong>"); // El elemento "<strong>" sólo se encuentra en la primera columna del triplete ("Dia")
+            if (intFirstPosition >= 0) // Los indices de IndexOf() empiezan en 0
+            {
+                intLastPosition = strCelda.IndexOf( "</strong>");
+                intFirstPosition = intFirstPosition + 8; 
+            }
+            else // Si el elemento "<strong>" no se ha encontrado, entonces estamos en la columna "Compra" o "Venta"
+            {
+                intFirstPosition = strCelda.IndexOf("tne10"); // la cadena 'class = "tne10"' sólo se encuentra en las columnas "Compra" o "Venta"
+                intFirstPosition = intFirstPosition + 8;
+                intLastPosition = strCelda.IndexOf("<", intFirstPosition);  // El caracter '<' fue dejado intencionalmente cuando se usó RegEx.Split() para poderse usarsele como marcador aquí
+            }
+
+            strValorCelda = strCelda.Substring(intFirstPosition, intLastPosition - intFirstPosition).Trim();
+            // Después de obtenerse las posiciones, se extrae la subcadena del valor
+
+            return strValorCelda;
+        }
+
+        // Agrega una fila en objDataTable
+        private void AddRow(System.Data.DataTable objDataTable, System.DateTime dtFecha, double dblCompra, double dblVenta)
+        {
+            System.Data.DataRow objDataRow;
+
+            objDataRow = objDataTable.NewRow();
+
+            objDataRow[0] = dtFecha;
+            objDataRow[1] = dblCompra;
+            objDataRow[2] = dblVenta;
+
+            objDataTable.Rows.Add(objDataRow);
+        }
+
+       
+    }
+}
